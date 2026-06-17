1f332de359c60a1556ecf1816fbdbedc7bc8ee02
Alfie <alfielytorres@gmail.com>
2026-06-17 16:01:44 +1000

    Fix metro.config.js: exclude react-native-maps from web bundle

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,4 +3,16 @@ const { withNativeWind } = require('nativewind/metro')
 
 const config = getDefaultConfig(__dirname)
 
+// Exclude react-native-maps from the web bundle (native-only module)
+const originalResolver = config.resolver.resolveRequest
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-maps') {
+    return { type: 'empty' }
+  }
+  if (originalResolver) {
+    return originalResolver(context, moduleName, platform)
+  }
+  return context.resolveRequest(context, moduleName, platform)
+}
+
 module.exports = withNativeWind(config, { input: './global.css' })
